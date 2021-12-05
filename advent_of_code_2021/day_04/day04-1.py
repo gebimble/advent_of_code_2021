f4f54893dd91c4d3d8e0d36d6662a9d810c82591
Joseph Beaver <joseph.beaver@gmail.com>
2021-12-05 23:40:32 +0000

    Incorrect day 4 solution, but working(ish) method.

--- advent_of_code_2021/day_04/day04-1.py
+++ advent_of_code_2021/day_04/day04-1.py
@@ -3,6 +3,36 @@ from scipy import stats
 
 from advent_of_code_2021.day_04 import numbers, bingo_cards
 
+def check_card(bingo_card: np.ndarray) -> bool:
+    """check_card.
 
-print(numbers)
-print(bingo_cards)
+    Parameters
+    ----------
+    bingo_card : np.ndarray
+        np.ndarray representing matches of a bingo card.
+
+    Returns
+    -------
+    bool
+        True if any row or column is all True in the matches array.
+
+    """
+    return any([any((bingo_card == True).all(axis=axis)) for axis in (0, -1)])
+
+boolean_cards = [np.full_like(x, fill_value=False, dtype=bool) for x in bingo_cards]
+
+def find_winner(numbers, bingo_cards, boolean_cards):
+    for number in numbers:
+        for i, card in enumerate(bingo_cards):
+            if (x:=np.where(card == number))[0].size > 0:
+                boolean_cards[i][x] = True
+
+        for j, boolean_card in enumerate(boolean_cards):
+            if check_card(boolean_card):
+                return bingo_cards[j], boolean_card, number
+
+winning_card, boolean_card, number = find_winner(numbers, bingo_cards, boolean_cards)
+print(winning_card, boolean_card)
+masked_array = np.ma.masked_array(winning_card, mask=boolean_card)
+print(masked_array, masked_array.sum(), number)
+print(masked_array.sum() * number)
