7ce24d913a34b18777313288ae25b0cb90604ce9
Joseph Beaver <joseph.beaver@gmail.com>
2021-12-21 17:29:08 +0000

    Finished day 1 tests.

--- tests/test_day_01.py
+++ tests/test_day_01.py
@@ -1,10 +1,15 @@
 import numpy as np
 
+import pytest
+
 from advent_of_code_2021.api.parsers.day_01 import delim_to_int_list
 from advent_of_code_2021.day_01.day_01 import increases, sum_increases
 
 
-def test_output_to_numpy(day_01_example_text_input):
+def test_output_to_numpy(day_01_example_text_input: pytest.fixture) -> np.ndarray:
+    # GIVEN the example text
+    # WHEN transformed into an integer list
+    # THEN the actual array should equal the expected arrray
     actual = delim_to_int_list(day_01_example_text_input)
 
     expected = np.array(
@@ -22,12 +27,15 @@ def test_output_to_numpy(day_01_example_text_input):
         ]
     )
 
-    assert actual
-    assert np.array_equal(expected, actual)
+    assert actual, 'No data pulled from input file.'
+    assert np.array_equal(expected, actual), 'Input file data differs from expected array.'
 
     return actual
 
-def test_increase_or_decrease(day_01_example_text_input):
+def test_increase_or_decrease(day_01_example_text_input: np.ndarray) -> (np.ndarray, np.ndarray):
+    # GIVEN the the content of the input file
+    # WHEN passed through the `increases` function
+    # THEN return an array that matches the expected output
     data_array = test_output_to_numpy(day_01_example_text_input)
 
     expected_output_string = [
@@ -42,17 +50,20 @@ def test_increase_or_decrease(day_01_example_text_input):
         'increased',
     ]
 
-    assert len(expected_output_string) == len(data_array)-1
+    assert len(expected_output_string) == len(data_array)-1, 'Array size mismatch.'
 
     expected = [x == 'increased' for x in expected_output_string]
 
     actual = increases(data_array)
 
-    assert np.array_equal(expected, actual)
+    assert np.array_equal(expected, actual), 'Output of `increase` does not match expected value.'
 
     return actual, expected
 
 def test_correct_sum(day_01_example_text_input):
+    # GIVEN the output arrays of `test_increase_or_decrease`
+    # WHEN summed
+    # THEN the sum value should equal the expected sum value
     data_array, increase_bool_array = test_increase_or_decrease(day_01_example_text_input)
 
     expect_sum = 7
